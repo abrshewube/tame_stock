e632c07dcde387ce95809fc74b0e041bd035b424
abrshewube <abrshefru1427@gmail.com>
2025-08-17 10:34:13 -0700

    added

--- client/src/services/saleService.ts
+++ client/src/services/saleService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/sales';
+const API_URL = 'https://tame-stock.onrender.com/api/sales';
 
 export interface SaleData {
   productId: string;
